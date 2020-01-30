54a36356ac98852729443535b431a240b494e3ec
Peter Clotworthy <me@sacredskull.net>
2020-01-30 15:08:14 +0000

    Generate short hash

--- TidalTests/upload-coverage.sh
+++ TidalTests/upload-coverage.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 CODACY_COVERAGE_VERSION="1.0.8"
+SHORT_HASH=$(git rev-parse --short "$GITHUB_SHA")
 
 curl -Ls "https://github.com/codacy/csharp-codacy-coverage/releases/download/$CODACY_COVERAGE_VERSION/Codacy.CSharpCoverage_linux-x64" --output Codacy.CSharpCoverage_linux-x64
 chmod +x ./Codacy.CSharpCoverage_linux-x64
-./Codacy.CSharpCoverage_linux-x64 -c $REPO_COMMIT_UUID -t $CODACY_PROJECT_TOKEN -r coverage/**/*.xml -e opencover
+./Codacy.CSharpCoverage_linux-x64 -c $GITHUB_SHA -t $SHORT_HASH -r coverage/**/*.xml -e opencover
