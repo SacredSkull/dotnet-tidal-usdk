9426f946e145d8389f4f8052331237ff46f7c9a3
Peter Clotworthy <me@sacredskull.net>
2020-01-30 15:32:47 +0000

    Remove script file and run inline

--- TidalTests/upload-coverage.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-CODACY_COVERAGE_VERSION="1.0.8"
-SHORT_HASH=$(git rev-parse --short "$GITHUB_SHA")
-
-curl -Ls "https://github.com/codacy/csharp-codacy-coverage/releases/download/$CODACY_COVERAGE_VERSION/Codacy.CSharpCoverage_linux-x64" --output Codacy.CSharpCoverage_linux-x64
-chmod +x ./Codacy.CSharpCoverage_linux-x64
-./Codacy.CSharpCoverage_linux-x64 -c $SHORT_HASH -t $CODACY_PROJECT_TOKEN -r coverage/**/*.xml -e opencover
